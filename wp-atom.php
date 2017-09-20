69129c51c6fdeacfd416a759dd6789cf66d16396
Florent Gallaire <fgallaire@gmail.com>
2017-09-20 04:10:31 +0200

    static export

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://jussieu.flext.net/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://jussieu.flext.net/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="http://jussieu.flext.net/feed/atom/">http://jussieu.flext.net/feed/atom/</a></p>
+	</body>
+</html>
---
+++ wp-content/themes/default/images/header-img.php
@@ -0,0 +1,2 @@
+<br />
+<b>Warning</b>:  imagejpeg(): Filename cannot be empty in <b>/home/flext/webapps/jussieu/wp-content/themes/default/images/header-img.php</b> on line <b>80</b><br />
